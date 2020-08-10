8f343619f9cfb9a6c4d18bd21ba4779ab0324981
liudehuang <2969878315@qq.com>
2020-08-10 18:08:59 +0800

    添加basic-java项目

---
+++ spring-boot-redis/src/main/java/com/liudehuang/boot/service/CacheService.java
@@ -0,0 +1,13 @@
+package com.liudehuang.boot.service;
+
+/**
+ * @Description:
+ * @Author: liudh
+ * @CreateDate: 2020/8/10 17:20
+ * @UpdateUser: liudh
+ * @UpdateDate: 2020/8/10 17:20
+ * @UpdateRemark:
+ * @Version:
+ */
+public interface CacheService {
+}
---
+++ spring-boot-redis/src/main/java/com/liudehuang/boot/service/impl/CacheServiceImpl.java
@@ -0,0 +1,15 @@
+package com.liudehuang.boot.service.impl;
+
+import com.liudehuang.boot.service.CacheService;
+
+/**
+ * @Description:
+ * @Author: liudh
+ * @CreateDate: 2020/8/10 17:21
+ * @UpdateUser: liudh
+ * @UpdateDate: 2020/8/10 17:21
+ * @UpdateRemark:
+ * @Version:
+ */
+public class CacheServiceImpl implements CacheService {
+}
